9b975f7cfe7d41f39d5b16c4c095e82399e2b228
Ditmaros <ditmarcastro@gmail.com>
2021-11-05 01:00:28 -0400

    Integrations continuos

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String message = "test de devopss";
+		String message = "test de devopss...";
 		Assert.assertNotNull(message);
 	}
 
